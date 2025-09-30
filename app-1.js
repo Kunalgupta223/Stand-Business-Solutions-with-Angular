bb8fab2920c743c847fef666449261bdfed0573e
Kunal Gupta <122199076+Kunalgupta223@users.noreply.github.com>
2025-09-30 14:38:22 +0530

    Add files via upload

---
+++ app-1.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Website loaded successfully!");
+});
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Our Process section loaded!");
+});
---
+++ app.js
@@ -0,0 +1,6 @@
+function scrollLeft() {
+      document.getElementById("capabilities").scrollBy({ left: -150, behavior: 'smooth' });
+    }
+    function scrollRight() {
+      document.getElementById("capabilities").scrollBy({ left: 150, behavior: 'smooth' });
+    }
